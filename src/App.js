9a6003f4e8ece5e889cc2fd79826686cfd799d2f
Zzang9u <146842085+Zzang9u@users.noreply.github.com>
2023-10-17 15:54:37 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import './App.css';
+import Movie from './Components/Movie/Movie';
+import { movies } from './movieDummy';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <div className="app-container">
+        {
+          movies.results.map((item) =>{
+            return (
+              <Movie
+              title = {item.title}
+              poster_path = {item.poster_path}
+              vote_average = {item.vote_average}
+              overview = {item.overview}
+              />
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
